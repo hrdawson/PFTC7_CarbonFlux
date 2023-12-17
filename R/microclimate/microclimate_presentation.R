6fb3ff73130f9c3e8ce47a9aafa3904828f950f9
Hilary Rose Dawson <hrosedawson@gmail.com>
2023-12-17 13:52:22 +0200

    Altered handling of metrics and created means

--- R/microclimate/microclimate_presentation.R
+++ R/microclimate/microclimate_presentation.R
@@ -2,6 +2,8 @@
 library(tidyverse)
 library(ggridges)
 library(tidylog)
+library(lubridate)
+library(rstatix)
 # For color gradient
 library(hrbrthemes)
 library(viridis)
@@ -9,18 +11,36 @@ library(viridis)
 # Read in Tomst data
 tomst = read.csv("clean_data/PFTC7_Tomst_Data.csv") |>
   # tidy
-  pivot_longer(cols = T1:moist, names_to = "variable", values_to = "value") |>
+  select(-moist) |>
+  pivot_longer(cols = T1:moist_vol, names_to = "metric", values_to = "value") |>
   # alter so that all temps have the same name
-  mutate(variable = case_when(
-    variable %in% c("T1", "T2", "T3") ~ "Temperature (C)",
-    variable == "moist" ~ "Soil moisture"
-  ))
+  mutate(metric = case_when(
+    metric == "T1" ~ "Aboveground Temperature (C)",
+    metric == "T2" ~ "Surface Temperature (C)",
+    metric == "T3" ~ "Soil Temperature (C)",
+    metric == "moist_vol" ~ "Soil moisture"
+  ),
+  metric = factor(metric, levels = c("Aboveground Temperature (C)", "Surface Temperature (C)",
+                                         "Soil Temperature (C)", "Soil moisture"))) |>
+  # work with date time for day/night
+  mutate(datetime = ymd_hms(datetime),
+         hour = hour(datetime),
+         day.night = case_when(
+           hour %in% 5:18 ~ "Day",
+           hour %in% 0:4 ~ "Night",
+           hour >= 19 ~ "Night"
+         ))
+
+tomst.means = tomst |>
+  group_by(metric, site, aspect, day.night) |>
+  get_summary_stats(value, type = "mean")
 
 # Make trial plot with basic Tomst data
 ggplot(tomst, aes(x = value, y = as.factor(site), fill = aspect)) +
   geom_density_ridges(alpha = 0.5) +
   theme_ridges() +
-  facet_grid(~variable, scales = "free_x") +
+  labs(y = "Site", x = "") +
+  facet_grid(day.night ~ metric, scales = "free_x") +
   theme(legend.position = "none")
 
 # make trial plot with basic FLIR
