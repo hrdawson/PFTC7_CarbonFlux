c0f8cd2293eadcc1fa3fc78a899d95a78ba3d9b7
Hilary Rose Dawson <hrosedawson@gmail.com>
2023-12-18 09:21:53 +0200

    Last minute changes

--- R/microclimate/make clean FLIR data for OSF.R
+++ R/microclimate/make clean FLIR data for OSF.R
@@ -1,10 +1,6 @@
 # Make clean FLIR data file for OSF
 
 # Flatten Rds to data frame ----
-FLIRraw = get(load("raw_data/flir_raw_2023-12-17.Rdata"))
-
-FLIRrawdata = FLIRraw[["raw_dat"]]
-
 FLIRraw = readRDS("raw_data/microclimate/FLIR_cropped.Rds") |>
   lapply(FUN = as.numeric)
 
@@ -29,4 +25,12 @@ FLIRflat = flir_long |>
   # Rename temp col
   rename(temp_C = value)
 
-write.csv(FLIRflat, "clean_data/flir_values.csv")
+# write.csv(FLIRflat, "clean_data/flir_values.csv")
+
+# make trial plot with basic FLIR
+ggplot(FLIRflat |> filter(temp_C > 0) |> drop_na(siteID),
+       aes(x = temp_C, y = as.factor(siteID), fill = aspect)) +
+  geom_density_ridges(alpha = 0.5) +
+  theme_ridges() +
+  facet_grid(~day.night, scales = "free_x") +
+  theme(legend.position = "none")
--- R/microclimate/microclimate_presentation.R
+++ R/microclimate/microclimate_presentation.R
@@ -8,11 +8,17 @@ library(rstatix)
 library(hrbrthemes)
 library(viridis)
 
-# Read in Tomst data
+# For standardised colors
+library(MetBrewer)
+
+scale_color_met_d(name = "Cassatt2")
+
+# Read in Tomst data ----
 tomst = read.csv("clean_data/PFTC7_Tomst_Data.csv") |>
   # tidy
   select(-moist) |>
   pivot_longer(cols = T1:moist_vol, names_to = "metric", values_to = "value") |>
+  rename(siteID = site) |>
   # alter so that all temps have the same name
   mutate(metric = case_when(
     metric == "T1" ~ "Aboveground Temperature (C)",
@@ -21,7 +27,14 @@ tomst = read.csv("clean_data/PFTC7_Tomst_Data.csv") |>
     metric == "moist_vol" ~ "Soil moisture"
   ),
   metric = factor(metric, levels = c("Aboveground Temperature (C)", "Surface Temperature (C)",
-                                         "Soil Temperature (C)", "Soil moisture"))) |>
+                                         "Soil Temperature (C)", "Soil moisture")),
+  elevation = case_when(
+    siteID == 1 ~ 2000,
+    siteID == 2 ~ 2200,
+    siteID == 3 ~ 2400,
+    siteID == 4 ~ 2600,
+    siteID == 5 ~ 2800
+  )) |>
   # work with date time for day/night
   mutate(datetime = ymd_hms(datetime),
          hour = hour(datetime),
@@ -29,44 +42,139 @@ tomst = read.csv("clean_data/PFTC7_Tomst_Data.csv") |>
            hour %in% 5:18 ~ "Day",
            hour %in% 0:4 ~ "Night",
            hour >= 19 ~ "Night"
-         ))
+         )) |>
+  mutate(dataset = "Tomst")
 
 tomst.means = tomst |>
-  group_by(metric, site, aspect, day.night) |>
-  get_summary_stats(value, type = "mean")
+  group_by(metric, siteID, day.night) |>
+  get_summary_stats(value, type = "mean_sd")
 
-# Make trial plot with basic Tomst data
-ggplot(tomst, aes(x = value, y = as.factor(site), fill = aspect)) +
-  geom_density_ridges(alpha = 0.5) +
+
+tomst.means.moist = tomst |>
+  group_by(metric, siteID, aspect) |>
+  get_summary_stats(value, type = "mean") |>
+  filter(metric == "Soil moisture") |>
+  select(-n) |>
+  pivot_wider(names_from = aspect, values_from = mean) |>
+  mutate(diff = east - west)
+
+# Make trial plot with basic Tomst data ----
+ggplot(tomst |> filter(metric != "Soil moisture"),
+       aes(x = value, y = as.factor(elevation), fill = aspect)) +
+  geom_density_ridges(alpha = 0.7, color = "grey50") +
   theme_ridges() +
+  scale_color_met_d(name = "Cassatt2", direction = - 1) +
+  scale_fill_met_d(name = "Cassatt2", direction = - 1) +
   labs(y = "Site", x = "") +
-  facet_grid(day.night ~ metric, scales = "free_x") +
-  theme(legend.position = "none")
+  facet_grid(metric ~ day.night, scales = "free_x")
 
-# make trial plot with basic FLIR
-ggplot(FLIRflat |> filter(temp_C > 0) |> drop_na(siteID),
-       aes(x = temp_C, y = as.factor(siteID), fill = aspect)) +
-  geom_density_ridges(alpha = 0.5) +
+ggsave("visualizations/2023.12.17_tempTomst.png", width = 10, height = 8, units = "in")
+
+ggplot(tomst |> filter(metric == "Soil moisture"),
+       aes(x = value, y = as.factor(elevation), fill = aspect)) +
+  geom_density_ridges(alpha = 0.7, color = "grey50") +
   theme_ridges() +
-  facet_grid(~day.night, scales = "free_x") +
-  theme(legend.position = "none")
+  scale_color_met_d(name = "Cassatt2", direction = - 1) +
+  scale_fill_met_d(name = "Cassatt2", direction = - 1) +
+  labs(y = "Site", x = "")
+  # facet_grid(day.night ~ ., scales = "free_x")
+
+ggsave("visualizations/2023.12.17_moistTomst.png", width = 6, height = 8, units = "in")
+
+# make trial plot with basic FLIR ----
+FLIRflat = read.csv("clean_data/flir_values.csv") |>
+  mutate(aspect = case_when(
+    siteID == 6 ~ "none",
+    TRUE ~ aspect
+  ),
+  aspect = factor(aspect, levels = c("east", "west", "none"))) |>
+  mutate(flag = case_when(
+    temp_C < 0 ~ "too cold",
+    day.night == "night" & temp_C > 18 ~ "too warm",
+    TRUE ~ "okay"
+  )) |>
+  filter(flag == "okay") |>
+  drop_na(siteID) |>
+  mutate(dataset = "FLIR",
+         elevation = case_when(
+           siteID == 1 ~ 2000,
+           siteID == 2 ~ 2200,
+           siteID == 3 ~ 2400,
+           siteID == 4 ~ 2600,
+           siteID == 5 ~ 2800,
+           siteID == 6 ~ 3000
+         ))
+
+FLIR.means = FLIRflat |>
+  group_by(siteID, day.night) |>
+  get_summary_stats(temp_C, type = "mean_sd")
+
+
+ggplot(FLIRflat, aes(x = temp_C, y = as.factor(elevation), fill = aspect, color = aspect)) +
+  geom_density_ridges(alpha = 0.7, color = "grey50") +
+  theme_ridges() +
+  scale_color_met_d(name = "Cassatt2", direction = - 1) +
+  scale_fill_met_d(name = "Cassatt2", direction = - 1) +
+  facet_grid(~day.night, scales = "free_x")
+
+ggsave("visualizations/2023.12.17_tempFLIR.png", width = 10, height = 8, units = "in")
 
 # Import IR temp gun data ----
 IRtemp = read.csv("raw_data/PFTC7_SA_raw_fluxes_2023.csv") |>
+  drop_na(siteID) |>
   # flag erroneous reads
   mutate(flag = case_when(
     str_detect(Remarks, "IRt on soil") ~ "discard",
     TRUE ~ "okay"
   )) |>
+  filter(flag == "okay") |>
+  # take most recent attempt
+  arrange(Attempt) |>
+  group_by(day.night, aspect, siteID, plotID) |>
+  slice_tail(n = 1) |>
+  ungroup() |>
   select(day.night:plotID, flag, IR_temp_1:IR_temp_5) |>
   pivot_longer(cols = IR_temp_1:IR_temp_5, names_to = "delete", values_to = "temp_C") |>
   drop_na(siteID) |>
-  filter(flag == "okay")
+  mutate(dataset = "IR Temp",
+         elevation = case_when(
+           siteID == 1 ~ 2000,
+           siteID == 2 ~ 2200,
+           siteID == 3 ~ 2400,
+           siteID == 4 ~ 2600,
+           siteID == 5 ~ 2800
+         ))
 
-# Plot IR temp gun data
+IRtemp.means = IRtemp |>
+  group_by(siteID, day.night) |>
+  get_summary_stats(temp_C, type = "mean_sd")
+
+# Plot IR temp gun data ----
 ggplot(IRtemp,
-       aes(x = temp_C, y = as.factor(siteID), fill = aspect)) +
-  geom_density_ridges(alpha = 0.5) +
+       aes(x = temp_C, y = as.factor(elevation), fill = aspect, color = aspect)) +
+  geom_density_ridges(alpha = 0.7, color = "grey50") +
   theme_ridges() +
-  facet_grid(~day.night, scales = "free_x") +
-  theme(legend.position = "none")
+  scale_color_met_d(name = "Cassatt2", direction = - 1) +
+  scale_fill_met_d(name = "Cassatt2", direction = - 1) +
+  facet_grid(~day.night, scales = "free_x")
+
+ggsave("visualizations/2023.12.17_IRtemps.png", width = 10, height = 8, units = "in")
+
+# Compare temps across methods ----
+
+temps = tomst |>
+  filter(metric == "Aboveground Temperature (C)") |>
+  rename(temp_C = value) |>
+  bind_rows(FLIRflat) |>
+  bind_rows(IRtemp) |>
+  select(day.night, siteID, aspect, plotID, dataset, temp_C) |>
+  mutate(day.night = str_to_title(day.night)) |>
+  mutate(aspect = factor(aspect, levels = c("east", "west", "none")))
+
+# plot
+ggplot(temps,
+       aes(x = temp_C, y = as.factor(siteID), fill = dataset, color = dataset)) +
+  geom_density_ridges(alpha = 0.7, color = "grey50") +
+  theme_ridges() +
+  facet_grid(~day.night, scales = "free_x")
+ggsave("visualizations/2023.12.17_tempMethods.png", width = 10, height = 8, units = "in")
