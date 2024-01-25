aaf26c8a571e7d9d7f7d08564295484aa3f8ba9e
hrdawson <hrosedawson@gmail.com>
2024-01-26 10:54:50 +1100

    Reconciling branches

--- R/microclimate/extract temps from li7500 measurements.R
+++ R/microclimate/extract temps from li7500 measurements.R
@@ -22,21 +22,51 @@ licor_files <- Map(c, co2fluxtent::read_files("raw_data/LI7500/LI7500_Site 1"),
 # Check if the files are ok
 licor_files <- test_flux_files(licor_files, skip = 3, min_rows = 50) ##removed three files
 
-print(licor_files)
+#Only the ambients
+licor_files_ambient <- licor_files[["ambient_names"]]
+filesAmbient <- licor_files_ambient |>
+  # Fixed missing file names thanks to this post https://stackoverflow.com/questions/69357657/purrrmap-dfr-gives-number-of-list-element-as-id-argument-not-value-of-list-e
+  purrr::set_names()
+
+#Read them in
+tempAmbient <- map_df(set_names(licor_files_ambient), function(file) {
+  file %>%
+    set_names() %>%
+    map_df(~ read_delim(file = file, skip = 3, delim = "\t")) #important! reading in American format
+}, .id = "File")
 
 # Gather site, plot etc. information from the filenames
+t.ambient = tempAmbient |>
+  # extract metadata
+  mutate(File = basename(File),
+         File = str_remove(File, ".txt")
+  ) |>
+  # Separate into relevant info
+  separate(File, into = c("flux", "siteID", "elevation", "aspect", "day.night"), remove = FALSE) |>
+  # Get just the relevant data
+  select(flux:day.night, Time:`Sequence Number`, `Temperature (C)`, `CO2 Signal Strength`) |>
+  # Filter to just data with a decent signal strength
+  filter(`CO2 Signal Strength` >= 90)
 
-meta <- tibble(file_path = unlist(licor_files),
-               file = basename(file_path)) %>%
-  mutate(site = unlist(lapply(file, function(x) str_split(x, "_")[[1]][1])),
-         elevation = unlist(lapply(file, function(x) str_split(x, "_")[[1]][2])),
-         aspect = unlist(lapply(file, function(x) str_split(x, "_")[[1]][3])),
-         plot = unlist(lapply(file, function(x) str_split(x, "_")[[1]][4])),
-         day_night = unlist(lapply(file, function(x) str_split(x, "_")[[1]][5])),
-         measurement = unlist(lapply(file, function(x) gsub(".txt","",tail(str_split(x, "_")[[1]],1)))),
-         redo = grepl("redo", file, ignore.case = T))
+# Summarise by the ten minute interval so that we can compare to Tomst ----
+library(lubridate)
+library(timetk)
 
-# Make list of just the ambients to read in
-filesAmbient = meta |>
-  filter(measurement == "a") |>
-  select(file_path)
+t.ten = t.ambient |>
+  mutate(datetime = paste0(Date, " ", Time),
+         datetime = str_sub(datetime, 1, 19),
+         datetime = ymd_hms(datetime)) |>
+  summarise_by_time(
+    .date_var = datetime,
+    .by = "minute",
+    temp.C = mean(`Temperature (C)`),
+    siteID = first(siteID),
+    elevation = first(elevation),
+    aspect = first(aspect),
+    day.night = first(day.night)
+  )
+
+# What if we skip all that and just work with the compiled data file ----
+# This will need to be redone when Michael finishes the function
+# It doesn't have all the data, but on the other hand, it has the data we've decided are "good" from a carbon standard
+# No, this doesn't work because it doesn't have the ambient.
