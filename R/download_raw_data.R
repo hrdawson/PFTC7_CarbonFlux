8c6077113ef8afd6f7a105f4244b1b6078d22a32
hrdawson <hrosedawson@gmail.com>
2024-01-29 12:58:48 +1100

    Added raw flux plot/metadata

--- R/download_raw_data.R
+++ R/download_raw_data.R
@@ -8,6 +8,17 @@ library(dataDownloader)
 
 
 # Tent flux (LI7500) data ----
+## LI7500 metadata (plot data) ----
+get_file(
+  # Which repository is it in?
+  node = "hk2cy",
+  # Which file do you want?
+  file = "PFTC7_SA_raw_fluxes_2023.csv",
+  # Where do you want the file to go to?
+  path = "raw_data/LI7500",
+  # Where is the file stored within the OSF repository?
+  remote_path = "raw_data/raw_flux_data/LI7500")
+
 ## Compiled LI7500 data ----
 get_file(
   # Which repository is it in?
